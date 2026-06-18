45e2e84a9f5923d5f04d5e4a6db75815894a2842
vinh <vinh.vo@futa.vn>
2026-06-18 08:26:53 +0800

    + Remove redundancy project

--- AgentSocietyChallenge_w_CrewAI/inspect_chroma.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="data/my_chroma")
-
-for c in client.list_collections():
-    col = client.get_collection(c.name)
-    print(c.name, col.count())
